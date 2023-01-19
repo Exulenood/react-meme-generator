8e505fa94bd8e91ff2521a1da17f6f2af3845979
Exulenood <119940832+Exulenood@users.noreply.github.com>
2023-01-19 22:47:07 +0100

    created UrlGenerator connected with useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+// function getMeme(memeTemplate, topText, bottomText) {
+//   const effMemeUrl = `https://api.memegen.link/images/${memeTemplate}/${topText}/${bottomText}.gif`;
+//   return effMemeUrl;
+// }
+
+export default function App() {
+  const [memeTemplate, setMemeTemplate] = useState('morpheus');
+  const [topText, setToptext] = useState(
+    'alright_code_-_I_may_have_broken_you',
+  );
+  const [bottomText, setBottomText] = useState('but_you_wont_break_me');
+  const [effMemeUrl, setEffMemeUrl] = useState();
+
+  useEffect(() => {
+    setEffMemeUrl(
+      `https://api.memegen.link/images/${memeTemplate}/${topText}/${bottomText}.gif`,
+    );
+  }, [memeTemplate, topText, bottomText]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {' '}
+      <button
+        onClick={() => {
+          console.log(effMemeUrl);
+        }}
+      >
+        DONT TOUCH
+      </button>
     </div>
   );
 }
-
-export default App;
+// next insert image with effMemeUrl (should be updating from start)
